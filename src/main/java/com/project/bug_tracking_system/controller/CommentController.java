6172a2e1762a5c535c672d43c3bd3eec0ca477c9
sarthaksohal <sarthaksohal03@gmail.com>
2025-09-04 19:54:26 +0530

    Code commit

--- src/main/java/com/project/bug_tracking_system/controller/CommentController.java
+++ src/main/java/com/project/bug_tracking_system/controller/CommentController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/comment")
-@CrossOrigin(origins = "https://bug-tracker-front-end.vercel.app/")
+@CrossOrigin(origins = "https://bug-tracker-front-end.vercel.app")
 public class CommentController {
 
     @Autowired
--- src/main/java/com/project/bug_tracking_system/controller/IssueController.java
+++ src/main/java/com/project/bug_tracking_system/controller/IssueController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/issue")
-@CrossOrigin(origins = "https://bug-tracker-front-end.vercel.app/")
+@CrossOrigin(origins = "https://bug-tracker-front-end.vercel.app")
 public class IssueController {
 
     @Autowired
